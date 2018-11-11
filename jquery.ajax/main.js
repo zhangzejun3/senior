0a18a52b7790ebcf210c9e559f9d07d40c4e4584
zzj101613 <zzj2472@163.com>
2018-11-11 20:44:27 +0800

    封装ajax

---
+++ jquery.ajax/main.js
@@ -0,0 +1,31 @@
+window.jQuery = function(nodeOrSelector){
+  let nodes = {}
+  nodes.addClass = function(){}
+  nodes.html = function(){}
+  return nodes
+}
+window.jQuery.ajax = function(url,method,body,successFn,failFn){
+  let request = new XMLHttpRequest()
+  request.open(method, url)
+  request.onreadystatechange = () => {
+    if (request.readyState === 4) {
+      if (request.status >= 200 && request.status < 300) {
+        successFn.call(undefined,request.responseText)
+      }else if(request.status >= 400){
+        failFn.call(undefined,request)
+      }
+    }
+  }
+  request.send(body)
+}
+window.$ = window.jQuery
+
+button.addEventListener('click', function (e) {
+  window.jQuery.ajax(
+    '/xxx',
+    'post',
+    'a=1&b=2',
+    (responseText) =>{ console.log(1) },
+    (request)=>{console.log(2)}
+  )
+})
